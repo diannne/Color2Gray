4e545c30633c743000b1072c16c6882e094855a8
Carlos Navarro <carlosnavapa@gmail.com>
2014-06-11 13:07:26 -0500

    Added MPI framework. Now the algorithm works in a cluster.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@ Venegas Munoz Carlos
 Navarro Palos Carlos Eduardo
 
 */
+#include <mpi/mpi.h>
 #include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include "ImageManipulator.h"
 #include "Definitions.h"
+
+#define NAME_LENGHT 11
 
 using namespace imageManipulator;
 using namespace std;
@@ -24,42 +30,82 @@ int main( int argc, char *argv[])
 {
     if ( argc != 2 )
     {
-        std::cout << "Please give me a file to use" << std::endl;
-        return -1 ;
+        cout << "Please give me a file to use" << endl;
+        return -1;
     }
 
-    std::string fileName(argv[1]);
-    std::string grayscaleFileName = fileName;
-    std::string sobelFileName = fileName;
-    std::string section1FileName = fileName;
-    std::string section2FileName = fileName;
+    ifstream hostsFile("hosts");
+    string line;
+    int numberHosts = 0;
+    if(hostsFile)
+    {
+        while (getline(hostsFile, line))
+        {
+            numberHosts++;
+        }
+    }
+    else
+    {
+        cout << "Could not read hosts file." << endl;
+        return -1;
+    }
+
+    string fileName(argv[1]);
+    string grayscaleFileName = fileName;
+    string sobelFileName = fileName;
+
+    int worldSize;
+    int worldRank;
+    char processorName[MPI_MAX_PROCESSOR_NAME];
+    int nameLength;
+
     grayscaleFileName.insert(fileName.size() - 4, "_grayscale");
     sobelFileName.insert(fileName.size() - 4, "_sobel");
-    section1FileName.insert(fileName.size() - 4, "_section1");
-    section2FileName.insert(fileName.size() - 4, "_section2");
 
-    ImageManipulator section1;
-    ImageManipulator section2;
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+
+    // Get the rank of the process
+    MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
+
+    // Get the name of the processor
+    MPI_Get_processor_name(processorName, &nameLength);
 
     ImageManipulator* imageManipulator = new ImageManipulator();
     imageManipulator->openImage(fileName.c_str());
 
 
     imageManipulator->convertToGrayscale();
-
     imageManipulator->saveImage(grayscaleFileName.c_str());
 
-    section1 = imageManipulator->applySobelsFilter(0, 0, imageManipulator->header.widht, imageManipulator->header.height / 2);
-    section1.saveImage(section1FileName.c_str());
-    section2 = imageManipulator->applySobelsFilter(0,  imageManipulator->header.height / 2, imageManipulator->header.widht, imageManipulator->header.height);
-    section1.saveImage(section2FileName.c_str());
 
-    /**
-    Only the clean up the borders. THis call is optional
-    **/
+    int sectionHeight = imageManipulator->header.height / numberHosts;
+    int sectionHeightBuffer = 0;
+
+    for(int i = 0; i < numberHosts; i++)
+    {
+        string section1FileName = fileName;
+        ImageManipulator imageSection;
+        char sectionName[NAME_LENGHT];
+        snprintf(sectionName, sizeof(sectionName), "%d", i);
+        section1FileName.insert(fileName.size() - 4, sectionName);
+
+        imageSection = imageManipulator->applySobelsFilter(0, sectionHeightBuffer, imageManipulator->header.widht, sectionHeightBuffer + sectionHeight);
+        imageSection.saveImage(section1FileName.c_str());
+
+        sectionHeightBuffer += sectionHeight;
+    }
+
+
+    //Only the clean up the borders. THis call is optional
     imageManipulator->applyBrihtness(50);
-
     imageManipulator->saveImage(sobelFileName.c_str());
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
 
     return 0;
 }
