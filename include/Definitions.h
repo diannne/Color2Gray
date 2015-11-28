5f997b59516e945108a2ad086f611f4e3d91c6af
Diana Popa <pdiana.maria91@gmail.com>
2015-11-28 19:42:34 +0200

    Towards experimental results

--- include/Definitions.h
+++ include/Definitions.h
@@ -7,36 +7,34 @@ CONCURRENT AND DISTRIBUTED PROGRAMMING 2014A
 Venegas Munoz Carlos
 Navarro Palos Carlos Eduardo
 
-*/
+ */
 #ifndef DEFINITIONS_H_INCLUDED
 #define DEFINITIONS_H_INCLUDED
 
 /*
 You must change these values in order to change the convolution matrix size, if not, you could break
 all the code because these constant are being used in a lot of parts of the code.
-*/
+ */
 #define CONVOLUTION_MATRIX_ROWS 3
 #define CONVOLUTION_MATRIX_COLUMNS 3
 /*
 THis file contains all the data type definitions neccesaries for Sobel and BMP.
-*/
+ */
 
 typedef unsigned char BYTE;
 typedef unsigned short WORD;
 typedef unsigned int DWORD;
 
-const int convolutionMatrixX[CONVOLUTION_MATRIX_ROWS][CONVOLUTION_MATRIX_COLUMNS] =
-{
-    {-1, -2, -1},
-    {0, 0, 0},
-    {1, 2, 1}
+const int convolutionMatrixX[CONVOLUTION_MATRIX_ROWS][CONVOLUTION_MATRIX_COLUMNS] ={
+	{-1, -2, -1},
+	{0, 0, 0},
+	{1, 2, 1}
 };
 
-const int convolutionMatrixY[CONVOLUTION_MATRIX_ROWS][CONVOLUTION_MATRIX_COLUMNS] =
-{
-    {-1, 0, 1},
-    {-2, 0, 2},
-    {-1, 0, 1}
+const int convolutionMatrixY[CONVOLUTION_MATRIX_ROWS][CONVOLUTION_MATRIX_COLUMNS] ={
+	{-1, 0, 1},
+	{-2, 0, 2},
+	{-1, 0, 1}
 };
 
 
--- include/ImageManipulator.h
+++ include/ImageManipulator.h
@@ -7,7 +7,7 @@ CONCURRENT AND DISTRIBUTED PROGRAMMING 2014A
 Venegas Munoz Carlos
 Navarro Palos Carlos Eduardo
 
-*/
+ */
 
 #ifndef IMAGEMANIPULATOR_H
 #define IMAGEMANIPULATOR_H
@@ -24,141 +24,136 @@ Navarro Palos Carlos Eduardo
 #define RGB2GRAY(r,g,b) (BYTE)( double(0.299) * r + double(0.587) * g + double(0.114) * b + 0.5)
 #define CLAMP(x)  (((x) > (255)) ? (255) : (((x) < (0)) ? (0) : (x)))
 
-namespace imageManipulator
-{
+namespace imageManipulator {
 
-/*
-This struct contains the all the data from the header of a BMP Image.
-You can check these documantation in: http://es.wikipedia.org/wiki/Windows_bitmap
-*/
-struct BMPHeader
-{
+	/*
+	This struct contains the all the data from the header of a BMP Image.
+	You can check these documantation in: http://es.wikipedia.org/wiki/Windows_bitmap
+	 */
+	struct BMPHeader {
+		WORD identifier;
+		DWORD size;
+		DWORD reserved;
+		DWORD offset;
+		DWORD headerSize;
+		DWORD height;
+		DWORD width;
+		WORD planes;
+		WORD bitsPerPixel;
+		DWORD compression;
+		DWORD imageSize;
+		DWORD horizontalResolution;
+		DWORD verticalResolution;
+		DWORD numberOfColours;
+		DWORD numberOfImportantColours;
+	};
 
-    WORD identifier;
-    DWORD size;
-    DWORD reserved;
-    DWORD offset;
-    DWORD headerSize;
-    DWORD height;
-    DWORD width;
-    WORD planes;
-    WORD bitsPerPixel;
-    DWORD compression;
-    DWORD imageSize;
-    DWORD horizontalResolution;
-    DWORD verticalResolution;
-    DWORD numberOfColours;
-    DWORD numberOfImportantColours;
-};
+	/*
+	THis class allow you to manipulate BMP images as matrix. Ypu can save
+	 * the header of the BMP and use it
+	for example to change the color mode, image size, etc.
 
+	You must first call openImage(const char* filePath) in order to turn on the
+	 * flag "imageLoaded" and then you can
+	manipulate the image.
 
-/*
-THis class allow you to manipulate BMP images as matrix. Ypu can save the header of the BMP and use it
-for example to change the color mode, image size, etc.
-
-You must first call openImage(const char* filePath) in order to turn on the flag "imageLoaded" and then you can
-manipulate the image.
-
-IMPORTANT: This class only works with BMP with 24 bits per pixel.
-*/
-class ImageManipulator
-{
-public:
+	IMPORTANT: This class only works with BMP with 24 bits per pixel.
+	 */
+	class ImageManipulator {
+	public:
 
 
-    ImageManipulator();
-    ~ImageManipulator();
-    ImageManipulator(const ImageManipulator& src);
-    /*
-    This method reads the file and checks that the file correspond
-    to an BMP and saves all the headers from this image.
-    */
-    bool openImage(const char* filePath);
-    /*
-    This method save the current header and the current imageDate in the file system
-    using the file path.
-    */
-    void saveImage(const char* filePath, bool grey=false);
-    
-    /*
-    This method convert all the pixels of the image in a grayscale.
-    */
-    void convertToGrayscale(const int initialX, 
-	const int initialY, const int finalX, const int finalY, int &, bool gooch=false);
-    
-    void traverseCIELab();
-    
-    double computeDelta(int i, int j);
-    
-    double crunch(double x);
-    void computeOutput();
-    void convertToRgb();
-    void SaveGreyData(const int initialX, 
-	const int initialY, const int finalX, const int finalY, int &);
-    
-    /*
-    This method changes the brightness of the entire image.
-    */
-    void applyBrightness(const int level);
-    /*
-    This method prints the header INFO
-    */
-    void printHeader();
-    /*
-    This method applies sobel filter to the image data in the given sector of the image.
-    This is usefull for distributing the job.
-    */
-    void applySobelsFilter(const int initialX, const int initialY, 
-    const int finalX, const int finalY, int &rv);
-    /*
-    Prints the image matrix following this form
-    (r,g,b)(r,g,b)...(r,g,b)
-    (r,g,b)(r,g,b)...(r,g,b)
-    ...
-    (r,g,b)(r,g,b)...(r,g,b)
+		ImageManipulator();
+		~ImageManipulator();
+		ImageManipulator(const ImageManipulator& src);
+		/*
+		This method reads the file and checks that the file correspond
+		to an BMP and saves all the headers from this image.
+		 */
+		bool openImage(const char* filePath);
+		/*
+		This method save the current header and the current imageDate in the file system
+		using the file path.
+		 */
+		void saveImage(const char* filePath, bool grey = false);
 
-    Since an image has a huge amount of pixels, for default only shows a matrix of 10x10
+		/*
+		This method convert all the pixels of the image in a grayscale.
+		 */
+		void convertToGrayscale(const int, const int, const int, const int, int &,
+				int gooch = 0);
 
-    */
-    void printDataImage(const bool isTest = true, const int maxTestSize = 10);
+		void convertTolab(const int, const int, const int, const int);
+		void computeTargetDifferences();
+		double computeDelta(int, int);
+		double crunch(double);
+		void resolveOptimizationProblem();
+		void convertToRgb(const int, const int, const int, const int);
+		void SetGreyOutput(const int, const int, const int, const int);
 
-//protected:
-//private:
+		/*
+		This method changes the brightness of the entire image.
+		 */
+		void applyBrightness(const int level);
+		/*
+		This method prints the header INFO
+		 */
+		void printHeader();
+		/*
+		This method applies sobel filter to the image data in the given sector of the image.
+		This is usefull for distributing the job.
+		 */
+		void applySobelsFilter(const int initialX, const int initialY,
+				const int finalX, const int finalY, int &rv);
+		/*
+		Prints the image matrix following this form
+		(r,g,b)(r,g,b)...(r,g,b)
+		(r,g,b)(r,g,b)...(r,g,b)
+		...
+		(r,g,b)(r,g,b)...(r,g,b)
 
-    FILE* imageFile; //The file where you load the image
-    BMPHeader header; //The header of the BMP
-    BYTE* imageData; //The image as a binary
-    BYTE* greyImageData; //The image as a binary
-    bool imageLoaded; //This flag allows you to use all the methods of this class.
-    bool isGrayscale; //True if the greyscale filter was applied to the image
-    CIE_Lab * _data;
-    double *_deltas;
-    int _radius;
-    double _theta;
-    double _alpha;
-    double * _dataOutput;
-    int _iterations;
-    Color *_greyOutput;
-    /*
-    This method returns a Color struct using the given x and y coordinates. The color struct
-    contains the pixel in RGB form. The programmer is responsable to check that the indexes are a valids
-    indexes.
-    */
-    Color getPixel(BYTE* imageData, const int x, const int y);
-    /*
-    This method put the the new pixel with the given color in the given coordinates.
-    The programmer is responsable to check that the indexes are a valids
-    indexes.
-    */
-    void setPixel(BYTE* imageData, const int x, const int y, const Color color);
-    
-    DWORD getHeight();
-    DWORD getWidth();
-    
-    void setHeight(DWORD);
-    
+		Since an image has a huge amount of pixels, for default only shows a matrix of 10x10
 
-};
+		 */
+		void printDataImage(const bool isTest = true, const int maxTestSize = 10);
+
+		//protected:
+		//private:
+
+		FILE* imageFile; //The file where you load the image
+		BMPHeader header; //The header of the BMP
+		BYTE* imageData; //The image as a binary
+		BYTE* greyImageData; //The image as a binary
+		bool imageLoaded; //This flag allows you to use all the methods of this class.
+		bool isGrayscale; //True if the greyscale filter was applied to the image
+		CIE_Lab * _data;
+		double *_deltas;
+		int _radius;
+		double _theta;
+		double _alpha;
+		double * _dataOutput;
+		int _iterations;
+		Color *_greyOutput;
+		/*
+		This method returns a Color struct using the given x and y coordinates. The color struct
+		contains the pixel in RGB form. The programmer is responsable to check that the indexes are a valids
+		indexes.
+		 */
+		Color getPixel(BYTE* imageData, const int x, const int y);
+		/*
+		This method put the the new pixel with the given color in the given coordinates.
+		The programmer is responsable to check that the indexes are a valids
+		indexes.
+		 */
+		void setPixel(BYTE* imageData, const int x, const int y, const Color color);
+
+		DWORD getHeight();
+		DWORD getWidth();
+
+		void setHeight(DWORD);
+
+
+	};
 
 }
 
